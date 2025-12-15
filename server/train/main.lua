86660a92a28393e76a8c39fa4946c76c1c9bbbff
CA.1 <mohammad92007@gmail.com>
2025-12-15 16:35:57 +0400

    Delete server/train directory

--- server/train/main.lua
+++
@@ -1,9 +0,0 @@
-QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent('mafia:spawnVehicle', function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if Player and Player.PlayerData.gang.name == Config.MafiaName then
-        TriggerClientEvent('mafia:openVehicleMenu', src)
-    end
-end)
